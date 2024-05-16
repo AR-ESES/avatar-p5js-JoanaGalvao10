c15b4e45862c1f31d661d3aa51d48a7994142f0d
JoanaGalvao10 <160878166+JoanaGalvao10@users.noreply.github.com>
2024-05-16 12:15:46 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,29 @@
 function setup() {
-  createCanvas(1280, 1024);
-  background(153);
-  stroke(0);
-  strokeWeight(2);
-  fill(237, 28, 36);
-  circle(30, 30, 40);
-  line(30, 30, 1280 / 2, 1024 / 2);
+  createCanvas(400, 400);
 }
 
-function draw() {}
+function draw() {
+  background(220);
+
+ 
+  fill(255, 204, 0);
+  ellipse(200, 150, 100, 100);
+  
+  fill(100, 25,0);
+  ellipse(175,140,20,20)
+  ellipse(225,140,20,20)
+
+
+  fill(0, 102, 153);
+  rect(175, 200, 50, 100);
+
+
+  fill(255, 0, 0);
+  rect(125, 200, 50, 20);
+  rect(225, 200, 50, 20);
+  
+  
+  fill(0,100,0);
+  rect(200,300,25,75);
+  rect(175,300,25,75);
+}
